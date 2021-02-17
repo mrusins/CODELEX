7c99c439a58a6ae0fdf15a39eac7fe7d0a17857d
mrusins <m.rusins@gmail.com>
2021-02-17 14:14:42 +0200

    update

--- CODELEX/exercises/basics/04.php
+++ CODELEX/exercises/basics/04.php
@@ -46,4 +46,3 @@ for ($x = 0; $x < count($superHumans->name); $x++) {
     echo 'My name is ' . $superHumans->name[$x] . ' ' . $superHumans->surname[$x] . '. I am '
         . $superHumans->age[$x] . ' and my birthday is ' . $superHumans->bd[$x] . ".\n";
 }
-//test
