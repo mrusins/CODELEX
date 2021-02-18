60c660bb1f7c447846c1577e5ea6af0a813521c1
mrusins <m.rusins@gmail.com>
2021-02-18 11:45:23 +0200

    update

---
+++ CODELEX/tasks/sheep.php
@@ -0,0 +1,18 @@
+<?php
+
+$animals = ['sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'sheep', 'sheep', 'wolf', 'sheep',];
+
+for ($i = 0; $i < count($animals) - 1; $i++) {
+
+    if ($animals[$i + 1] === 'wolf') {
+        echo "OMG,";
+    } else if ($animals[$i] === 'wolf') {
+        echo 'HEHE,';
+    } else {
+        echo 'sheep, ';
+    }
+
+
+}
+
+
