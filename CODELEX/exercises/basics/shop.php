0fea3ec1c274e072931d18d218f52fd8dbb02661
mrusins <m.rusins@gmail.com>
2021-02-18 10:02:06 +0200

    update

--- CODELEX/exercises/basics/shop.php
+++ CODELEX/exercises/basics/shop.php
@@ -29,21 +29,24 @@ $list = [
 function getProductName(array $list): void
 {
     for ($i = 0; $i < count($list); $i++) {
-        echo $list[$i]['name'] . ":" . $i . "\n";
+        echo $list[$i]['name'] . ":" . $i . PHP_EOL;
     }
 }
 
-echo "\n\n";
+PHP_EOL;
 $productNr = readline(getProductName($list) . "\nEnter a product Nr: ");
 if ($productNr >= count($list)) {
-    die("Wrong ID, try again \n");
+    die("Wrong ID, try again" . PHP_EOL);
 }
 $quantity = readline('Enter a quantity: ');
+if (is_numeric($quantity) != true) {
+    die("Enter only numbers!" . PHP_EOL);
+}
 
 
 function countInBasket(array $list, int $productNr, int $quantity): string
 {
-    return 'In your cart is ' . $quantity . ' of ' . $list[$productNr]['name'] . ' with total sum ' . $list[$productNr]['price'] * $quantity . "EUR\n";
+    return 'In your cart is ' . $quantity . ' of ' . $list[$productNr]['name'] . ' with total sum ' . $list[$productNr]['price'] * $quantity . "EUR" . PHP_EOL;
 }
 
 echo countInBasket($list, $productNr, $quantity);
