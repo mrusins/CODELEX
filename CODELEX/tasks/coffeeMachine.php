812e22f51b2d80a8ebdfd987cede1b59e1fcbbb7
mrusins <m.rusins@gmail.com>
2021-02-22 19:17:06 +0200

    update

--- CODELEX/tasks/coffeeMachine.php
+++ CODELEX/tasks/coffeeMachine.php
@@ -1,21 +1,37 @@
 <?php
 print("\033[2J\033[;H");
 $test = 0;
+//TODO: choose wallet
 $wallet = [1 => 10, 2 => 10, 10 => 10, 20 => 10, 50 => 10, 100 => 10, 200 => 10];
+//$wallet = [1 => 10, 2 => 10];
 $onlyNominals = [];
 $priceOfChoosedDrink = 0;
 $sumOfInsertedCoins = 0;
+$totalinWallet = 0;
 $menu = ['Coffee' => 200, 'Tea' => 120, 'Hot Water' => 60];
 
-function countTotal(array $wallet, array &$onlyNominals): int
+function countTotal(array $wallet, array &$onlyNominals, int &$totalinWallet): int
 {
     $sum = 0;
     foreach ($wallet as $key => $value) {
         $sum += $key * $value;
         array_push($onlyNominals, $key);
     }
+    $totalinWallet = $sum;
     return $sum;
 }
+countTotal($wallet, $onlyNominals, $totalinWallet);
+
+function isMoneyEnought(array $menu, int $totalinWallet): void
+{
+    foreach ($menu as $name => $price) {
+        if ($totalinWallet < $price) {
+            exit('Not enough money in your wallet!' . PHP_EOL);
+        }
+    }
+
+}
+isMoneyEnought($menu, $totalinWallet);
 
 function walletInventorization(array $wallet): void
 {
@@ -33,20 +49,23 @@ function printMenu(array $menu, int &$priceOfChoosedDrink): void
         $index++;
         echo "[$index] " . $key . " EUR " . $value / 100 . PHP_EOL;
     }
+
     $choice = readline('Enter: ');
-    if ($choice == 1) {
+    if ($choice != '1' && $choice != '2' && $choice != '3') {
+        exit('Wrong choice!' . PHP_EOL);
+    } elseif ($choice == 1) {
         $priceOfChoosedDrink = $menu['Coffee'];
-    }
-    if ($choice == 2) {
+    } elseif ($choice == 2) {
         $priceOfChoosedDrink = $menu['Tea'];
-    }
-    if ($choice == 3) {
+    } elseif ($choice == 3) {
         $priceOfChoosedDrink = $menu['Hot Water'];
     }
 
 }
 
+echo 'In your wallet is ' . 'EUR ' . ($totalinWallet / 100) . PHP_EOL;
 printMenu($menu, $priceOfChoosedDrink);
+var_dump($totalinWallet);
 
 
 function insertCoins(array &$wallet, int &$sumOfInsertedCoins, int $incertedCoin, array $onlyNominals): void
@@ -60,25 +79,25 @@ function insertCoins(array &$wallet, int &$sumOfInsertedCoins, int $incertedCoin
 //        }
 //        $localCount++;
 //}
-    if ($incertedCoin == 1) {
+    if ($incertedCoin == 1 && $wallet['1'] > 0) {
         $wallet['1']--;
         $sumOfInsertedCoins += $onlyNominals[0];
-    } elseif ($incertedCoin == 2) {
+    } elseif ($incertedCoin == 2 && $wallet['2'] > 0) {
         $wallet['2']--;
         $sumOfInsertedCoins += $onlyNominals[1];
-    } elseif ($incertedCoin == 3) {
+    } elseif ($incertedCoin == 3 && $wallet['10'] > 0) {
         $wallet['10']--;
         $sumOfInsertedCoins += $onlyNominals[2];
-    } elseif ($incertedCoin == 4) {
+    } elseif ($incertedCoin == 4 && $wallet['20'] > 0) {
         $wallet['20']--;
         $sumOfInsertedCoins += $onlyNominals[3];
-    } elseif ($incertedCoin == 5) {
+    } elseif ($incertedCoin == 5 && $wallet['50'] > 0) {
         $wallet['50']--;
         $sumOfInsertedCoins += $onlyNominals[4];
-    } elseif ($incertedCoin == 6) {
+    } elseif ($incertedCoin == 6 && $wallet['100'] > 0) {
         $wallet['100']--;
         $sumOfInsertedCoins += $onlyNominals[5];
-    } elseif ($incertedCoin == 7) {
+    } elseif ($incertedCoin == 7 && $wallet['200'] > 0) {
         $wallet['200']--;
         $sumOfInsertedCoins += $onlyNominals[6];
     }
@@ -86,7 +105,7 @@ function insertCoins(array &$wallet, int &$sumOfInsertedCoins, int $incertedCoin
 
 do {
     print("\033[2J\033[;H");
-    echo 'In your wallet is ' . 'EUR ' . countTotal($wallet, $onlyNominals) / 100 . PHP_EOL . 'Total of inserd: EUR ' . $sumOfInsertedCoins . PHP_EOL;
+    echo 'In your wallet is ' . 'EUR ' . countTotal($wallet, $onlyNominals, $totalinWallet) / 100 . PHP_EOL . 'Total of inserted: EUR ' . ($sumOfInsertedCoins / 100) . PHP_EOL;
 
     walletInventorization($wallet);
     $incertedCoin = readline('Enter: ');
@@ -98,6 +117,6 @@ print("\033[2J\033[;H");
 $change = $sumOfInsertedCoins - $priceOfChoosedDrink;
 
 echo 'Enjoy your drink' . PHP_EOL . 'Your change is: EUR ' . ($change / 100) . PHP_EOL .
-    'You still have EUR ' . (countTotal($wallet, $onlyNominals) + $change) / 100 . PHP_EOL;
+    'You still have EUR ' . (countTotal($wallet, $onlyNominals, $totalinWallet) + $change) / 100 . PHP_EOL;
 
 
