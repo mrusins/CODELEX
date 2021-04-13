720aff13b5cc62d34781617eb4a70bd386c38134
mrusins <m.rusins@gmail.com>
2021-04-13 18:50:47 +0300

    Person Registry

--- CODELEX/tasks/Stock-shop/app/Repositories/MySQLRepository.php
+++ CODELEX/tasks/Stock-shop/app/Repositories/MySQLRepository.php
@@ -17,7 +17,7 @@ class MySQLRepository implements DBRepository
         $pdo = new PDOConnector(
             'localhost', // server
             'maris',      // user
-            'maris1234',      // password
+            '',      // password
             'Stocks'   // database
         );
         $pdoConn = $pdo->connect('utf8', []); // charset, options
