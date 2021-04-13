4f7d62ecebaa54ed5017479e67803c42b20203e0
mrusins <m.rusins@gmail.com>
2021-04-13 18:56:43 +0300

    Person Registry

--- CODELEX/tasks/Stock-shop/app/Repositories/StocksAPIRepository.php
+++ CODELEX/tasks/Stock-shop/app/Repositories/StocksAPIRepository.php
@@ -6,17 +6,20 @@ namespace App\Repositories;
 class StocksAPIRepository
 {
 
+    private string $token =''; //TODO add your private token
+
     public function getNIOCurrentPrice()
     {
-        $url = 'https://finnhub.io/api/v1/quote?symbol=NIO&token='; //TODO add token
+        $url = "https://finnhub.io/api/v1/quote?symbol=$this->token";
         $json = file_get_contents($url);
         $data = json_decode($json, TRUE);
         return $data['c'];
+
     }
 
     public function searchStock(string $apiName): array
     {
-        $url = "https://finnhub.io/api/v1/quote?symbol=" . $apiName . "&token="; //TODO add token
+        $url = "https://finnhub.io/api/v1/quote?symbol=" . $apiName . "&token=$this->token";
         $json = file_get_contents($url);
         $data = json_decode($json, TRUE);
         return $data;
